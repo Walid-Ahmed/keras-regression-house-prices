ff90a5d0592fea22416ee118b32380585b77a6b0
Walid Ahmed <walidahmed@Walids-MacBook-Air.local>
2019-10-14 19:28:52 -0400

    intial commit

---
+++ pyimagesearch/models.py
@@ -0,0 +1,24 @@
+# import the necessary packages
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Dropout
+from keras.layers.core import Dense
+from keras.layers import Flatten
+from keras.layers import Input
+from keras.models import Model
+
+def create_mlp(dim, regress=False):
+	# define our MLP network
+	model = Sequential()
+	model.add(Dense(8, input_dim=dim, activation="relu"))
+	model.add(Dense(4, activation="relu"))
+
+	# check to see if the regression node should be added
+	if regress:
+		model.add(Dense(1, activation="linear"))
+
+	# return our model
+	return model
